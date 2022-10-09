b478a2ee8bcf5928feb13272e21b79cd9a038906
aliabbas <alixaidi1577@gmail.com>
2022-10-09 11:12:59 +0500

    commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// const name=[
+// "ali",
+// "abbas",
+// ];
+// console.log(name[0]);
+
+
+
+// function welcome(){
+
+// console.log(sum(2,4));
+    
+// var sum=(num1,num2)=>{
+//     return num1*num2;
+// };
+
+// //or 
+// const mul=(num1,num2)=>num1*num2;
+// console.log(mul);
+
+
+
+
+
+// const product={
+//     p_name:"computer",
+//     p_price:20000,
+//     //method function should be a normal function, subsequent functions can be Arrow Function
+//     ProductName:function(callback){
+//         //.....use when we don't want to create new scope..... 
+        
+//         //  return function(){//simple function
+//         //          return `your name ${this.name}`;//this points to (global) window
+//         //  }
+
+//         //.......Arrow Function.......
+//         return ()=>`Product Name is ${this.p_name} and Price is ${this.p_price}`;
+//         //order to retain the value & same scope as outside
+//     },
+        
+// }
+// const p1=product.ProductName();
+// console.log(p1());
+
+
+
+// const assignvar=(callback)=>{
+// setTimeout(() => {
+//     callback("response");
+// }, 3000);
+
+// };
+// assignvar(responseP);
+// module.exports={
+//     xx:10,
+//     ks:200,
+// }
+
+console.log("js");
